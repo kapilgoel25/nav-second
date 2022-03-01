ad97bae50172e891ec5605787498c03400ea9fe2
kapilgoel25 <kapilgoel533@gmail.com>
2022-03-01 23:33:17 +0530

    nav-sec first

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  Navigation,
+  Footer,
+  Home,
+  About,
+  Contact,
+  Blog,
+  Posts,
+  Post
+} from "./components";
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />}>
+          <Route path="" element={<Posts />} />
+          <Route path=":postSlug" element={<Post />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
